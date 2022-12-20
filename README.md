1e5cc0708f30cccf58122443dae2fda412315b31
Jonathan Coates <git@squiddev.cc>
2022-12-20 09:38:30 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# netherite-tools
+A datapack which adds Netherite tools to ComputerCraft. They don't have any benefits over diamond tools, as turtles
+instabreak blocks and don't consume durability, but are a great show of opulence.
+
+## Usage
+ - Download the pack from the "Releases" tab.
+ - Add it to your world's datapacks folder.
+ - Enable the pack with `/datapack enable`
---
+++ make.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import json
+from io import TextIOWrapper
+from zipfile import ZipFile
+
+tools: dict[str, dict] = {
+    "minecraft:netherite_axe": {
+        "type": "computercraft:tool",
+        "item": "minecraft:netherite_axe",
+        "adjective": "upgrade.minecraft.diamond_axe.adjective",
+        "damageMultiplier": 6,
+    },
+    "minecraft:netherite_pickaxe": {
+        "type": "computercraft:tool",
+        "item": "minecraft:netherite_pickaxe",
+        "adjective": "upgrade.minecraft.diamond_pickaxe.adjective",
+    },
+    "minecraft:netherite_hoe": {
+        "type": "computercraft:tool",
+        "item": "minecraft:netherite_hoe",
+        "adjective": "upgrade.minecraft.diamond_hoe.adjective",
+        "breakable": "computercraft:turtle_hoe_harvestable",
+    },
+    "minecraft:netherite_shovel": {
+        "type": "computercraft:tool",
+        "item": "minecraft:netherite_shovel",
+        "adjective": "upgrade.minecraft.diamond_shovel.adjective",
+        "breakable": "computercraft:turtle_shovel_harvestable",
+    },
+    "minecraft:netherite_sword": {
+        "type": "computercraft:tool",
+        "item": "minecraft:netherite_sword",
+        "adjective": "upgrade.minecraft.diamond_sword.adjective",
+        "breakable": "computercraft:turtle_sword_harvestable",
+        "damageMultiplier": 9,
+    },
+}
+
+extra_files = [
+    "pack.mcmeta",
+]
+
+with ZipFile("netherite-tools.zip", "w") as zip:
+    for id, tool in tools.items():
+        mod, path = id.split(":", maxsplit=1)
+        with zip.open(f"data/{mod}/computercraft/turtle_upgrades/{path}.json", "w") as out:
+            with TextIOWrapper(out) as wrapper:
+                json.dump(tool, wrapper)
+
+    for file in extra_files:
+        with open(file, "rb") as input:
+            zip.write(file)
